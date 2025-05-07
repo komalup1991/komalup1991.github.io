ba555f75890bb8de05430524ecc5123fe471d2b3
Komal12 <upadhyay.ko@northeastern.edu>
2025-05-07 01:29:16 -0700

    updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/komalup1991.github.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
